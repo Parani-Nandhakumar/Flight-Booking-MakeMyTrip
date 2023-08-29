4592b54b8f9dbec1d6188f34af1b0f65182af4f7
Parani-Nandhakumar <parani777@gmail.com>
2023-08-29 22:14:46 +0530

    Added few code

--- src/test/java/FlightTicketBooking.java
+++ src/test/java/FlightTicketBooking.java
@@ -1,8 +1,6 @@
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FlightTicketBooking {
     public static void main(String[] args) throws InterruptedException {
@@ -22,5 +20,13 @@ public class FlightTicketBooking {
         }
         Thread.sleep(2000);
         driver.findElement(By.cssSelector("#username")).sendKeys("parani777@gmail.com");
-    }
+        driver.findElement(By.cssSelector(".btnContainer.appendBottom25")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#password")).sendKeys("Kalamani@1969");
+        driver.findElement(By.cssSelector(".btnContainer.appendBottom25")).click();
+        Thread.sleep(1000);
+        Actions action = new Actions(driver);
+        WebElement makeMyTripPageElement = driver.findElement(By.cssSelector(".mmtLogo.makeFlex"));
+        action.moveToElement(makeMyTripPageElement).click(makeMyTripPageElement).build().perform();
+}
 }
